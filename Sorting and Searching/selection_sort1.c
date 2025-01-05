840306a19f74875d1b1724c765153b0e00495368
Vishal Kompalli <vishal.kompalli@gmail.com>
2025-01-05 20:19:53 +0530

    generic selection sort added

---
+++ Sorting and Searching/selection_sort1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void selection_sort(int arr[], int n)
+{
+    int min, temp=0,i=0;
+    for(i = 0;i < n-1;i++)
+    {
+        min = i;
+        for(int j=i+1; j<n;j++)
+        {
+            if(arr[j] < arr[min])
+            {
+                min  = j;
+            }
+
+            temp = arr[i];
+            arr [i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    
+}
+
+void printarray(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    int *arr;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    // int arr[n];
+    printf("Enter %d elements: ",n);
+    arr = (int*)malloc(sizeof(int)*n);
+    {
+        if(arr == NULL)
+        {
+            printf("Memory Allocation failed\n");
+            return 1;
+        }
+        
+    }
+    {
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+    }
+    selection_sort(arr,n);
+    printf("Selection sorted array is: ");
+    printarray(arr,n);
+    free(arr);
+    return 0;
+
+}
