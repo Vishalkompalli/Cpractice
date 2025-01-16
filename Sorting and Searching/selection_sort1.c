e72eddf238cf495c6376ade17a859a7b0470e95e
Vishal Kompalli <vishal.kompalli@gmail.com>
2025-01-16 19:40:37 +0530

    Comments updated

--- Sorting and Searching/selection_sort1.c
+++ Sorting and Searching/selection_sort1.c
@@ -6,7 +6,7 @@ void selection_sort(int arr[], int n)
     int min, temp=0,i=0;
     for(i = 0;i < n-1;i++)
     {
-        min = i;
+        min = i;//initially, 1st element is the minimum
         for(int j=i+1; j<n;j++)
         {
             if(arr[j] < arr[min])
@@ -14,7 +14,7 @@ void selection_sort(int arr[], int n)
                 min  = j;
             }
 
-            temp = arr[i];
+            temp = arr[i];//swapping i(th) and minimum elements
             arr [i] = arr[min];
             arr[min] = temp;
         }
