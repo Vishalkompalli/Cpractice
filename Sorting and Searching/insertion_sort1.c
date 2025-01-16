710f63356f4fa55d17083faa3ed7ca931567070e
Vishal Kompalli <vishal.kompalli@gmail.com>
2025-01-16 11:40:18 +0530

    Comments updated

--- Sorting and Searching/insertion_sort1.c
+++ Sorting and Searching/insertion_sort1.c
@@ -5,17 +5,17 @@ void insertion_sort(int arr[], int n)
 {
     for (int i = 1; i < n; i++) 
     {
-        int key = arr[i];
-        int j = i - 1;
+        int key = arr[i];// 2nd element(The element with index 1) is the key
+        int j = i - 1;// j is the variable used to traverse the array - starts at the 1st position
 
          /*Move elements of arr[0..i-1] that are greater than `key`
          to one position ahead of their current position*/
         while (j >= 0 && arr[j] > key) 
         {
-            arr[j + 1] = arr[j];
-            j--;
+            arr[j + 1] = arr[j];//moving the jth element to (j+1)th position
+            j--;// j is decremented because the condition in while has to be false (Because only one iteration of the loop is needed)
         }
-        arr[j + 1] = key;
+        arr[j + 1] = key;// The key is put back at whatever position it is supposed to go
     }
 }
 
